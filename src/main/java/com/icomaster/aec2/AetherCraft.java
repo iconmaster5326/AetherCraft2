9d41971c75d08489f99940578add69eaaade1d59
Iconmaster <crazybonesj@yahoo.com>
2015-02-06 19:40:09 -0500

    Initial commit

---
+++ src/main/java/com/icomaster/aec2/AetherCraft.java
@@ -0,0 +1,17 @@
+package com.icomaster.aec2;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = AetherCraft.MODID, version = AetherCraft.VERSION)
+public class AetherCraft {
+
+	public static final String MODID = "AetherCraft2";
+	public static final String VERSION = "1.0";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+}
