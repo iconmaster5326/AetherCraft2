d75bf34819eb969bf80d25d1da86d9f1bcbe2c81
Iconmaster <crazybonesj@yahoo.com>
2015-02-08 15:27:34 -0500

    PRETTY PICTURES

--- src/main/java/com/iconmaster/aec2/client/gui/GuiCrucible.java
+++ src/main/java/com/iconmaster/aec2/client/gui/GuiCrucible.java
@@ -1,12 +1,15 @@
 package com.iconmaster.aec2.client.gui;
 
+import com.iconmaster.aec2.aether.Compound;
 import com.iconmaster.aec2.gui.AetherCraftContainer;
 import com.iconmaster.aec2.network.AetherCraftPacketHandler;
 import com.iconmaster.aec2.network.RequestHeatSyncPacket;
 import com.iconmaster.aec2.te.AetherCraftTE;
 import com.iconmaster.aec2.te.TECrucible;
+import java.util.Random;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 /**
  *
@@ -27,5 +30,25 @@ public class GuiCrucible extends AetherCraftGui<TECrucible> {
 		super.drawGuiContainerForegroundLayer(mouseX, mouseY);
 		
 		fontRendererObj.drawString("Heat: "+te.realHeat, 7, 54, 0x404040);
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_) {
+		super.drawGuiContainerBackgroundLayer(p_146976_1_, p_146976_2_, p_146976_3_);
+		
+		int x = (this.width - this.xSize) / 2;
+		int y = (this.height - this.ySize) / 2;
+		
+		int i=0;
+		for (Compound c : te.reactor.reactants) {
+			Random r = new Random(i+c.hashCode());
+			
+			GL11.glColor3b((byte) (c.color&0x0000ff),(byte) ((c.color&0x00ff00)>>8),(byte) ((c.color&0xff0000)>>16));
+			drawTexturedModalRect(x+68+r.nextInt(105-68-16), y+15+r.nextInt(70-15-16), 240, 57, 16, 16);
+			i++;
+		}
+		
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		drawTexturedModalRect(x+68, y+15, 218, 0, 105-68, 70-15);
 	}
 }
