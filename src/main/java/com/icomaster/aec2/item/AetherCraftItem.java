5e34a6db2039c7f9af71c32f4cd7e3b5a27b27d1
Iconmaster <crazybonesj@yahoo.com>
2015-02-07 19:13:45 -0500

    Add item stub

--- src/main/java/com/icomaster/aec2/item/AetherCraftItem.java
+++ src/main/java/com/icomaster/aec2/item/AetherCraftItem.java
@@ -1,11 +1,43 @@
 package com.icomaster.aec2.item;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 
 /**
  *
  * @author iconmaster
  */
 public class AetherCraftItem extends Item {
+	public String name;
+	public boolean hasMeta = false;
+	public int metas;
+	public ItemTextures textures;
+
+	public AetherCraftItem(String name, ItemTextures textures, int metas) {
+		this.name = name;
+		this.metas = metas;
+		this.textures = textures;
+		this.hasMeta = true;
+	}
 	
+	public AetherCraftItem(String name, ItemTextures textures) {
+		this.name = name;
+		this.textures = textures;
+	}
+	
+	public void register() {
+		GameRegistry.registerItem(this,name);
+	}
+
+	@Override
+	public String getUnlocalizedName() {
+		return "aec.item."+name;
+	}
+
+	@Override
+	public IIcon getIcon(ItemStack stack, int pass) {
+		return textures.getTexture(stack.getItemDamage());
+	}
 }
---
+++ src/main/java/com/icomaster/aec2/item/ItemTextures.java
@@ -0,0 +1,38 @@
+package com.icomaster.aec2.item;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+
+/**
+ *
+ * @author iconmaster
+ */
+public class ItemTextures {
+	public String[] names;
+	public IIcon[] icons;
+	public boolean hasMeta = true;
+
+	public ItemTextures(String[] names) {
+		this.names = names;
+	}
+	
+	public ItemTextures(String names) {
+		this.names = new String[] {names};
+		hasMeta = false;
+	}
+	
+	public void register(IIconRegister iconRegister) {
+		icons = new IIcon[names.length];
+		for (int i=0;i<names.length;i++) {
+			icons[i] = iconRegister.registerIcon(names[i]);
+		}
+	}
+	
+	public IIcon getTexture(int meta) {
+		if (hasMeta) {
+			return icons[meta];
+		} else {
+			return icons[0];
+		}
+	}
+}
